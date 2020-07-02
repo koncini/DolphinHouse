2fa99a84ce152996da490e56dbdc1d6636905862
koncini <juan.rincon8205@gmail.com>
2020-07-02 16:16:59 -0500

    Soporte propiedades de texto

---
+++ src/main/java/com/dolphinhouse/dolphinhouse/TextPropertiesConfig.java
@@ -0,0 +1,9 @@
+package com.dolphinhouse.dolphinhouse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({@PropertySource("classpath:text.properties")})
+public class TextPropertiesConfig {}
