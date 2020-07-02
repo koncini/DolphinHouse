e1bc650d7e290ec5123a410f049ed1f8fb66c19f
koncini <juan.rincon8205@gmail.com>
2020-07-02 16:43:01 -0500

    Creando lista de productos

--- src/main/java/com/dolphinhouse/dolphinhouse/contollers/IndexController.java
+++ src/main/java/com/dolphinhouse/dolphinhouse/contollers/IndexController.java
@@ -1,13 +1,19 @@
 package com.dolphinhouse.dolphinhouse.contollers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/index")
-    public String index() {
+    @Value("${text.indexcontroller.index.title}")
+    private String indexText;
+
+    @RequestMapping({"/index", "", "/"})
+    public String index(Model model) {
+        model.addAttribute("title", indexText);
         return "index";
     }
 
--- src/main/java/com/dolphinhouse/dolphinhouse/contollers/ProductController.java
+++ src/main/java/com/dolphinhouse/dolphinhouse/contollers/ProductController.java
@@ -4,6 +4,7 @@ import com.dolphinhouse.dolphinhouse.models.domain.Product;
 import com.dolphinhouse.dolphinhouse.models.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,29 +15,36 @@ import java.util.List;
 @RequestMapping("catalog")
 public class ProductController {
 
+    @Value("${text.productController.catalog}")
+    private String catalogText;
+
     @Autowired
     private ProductService productService;
 
     @GetMapping("/products")
     public String getAllProducts(Model model) {
         List<Product> productList = productService.getAllProducts();
-        model.addAttribute("lista", productList);
+        model.addAttribute("list", productList);
+        model.addAttribute("title", catalogText);
         return "catalog";
     }
 
     @PostMapping("/addProduct")
-    public void addProduct(@RequestBody Product product) {
+    public String addProduct(@RequestBody Product product, Model model) {
         productService.addProduct(product);
+        return "";
     }
 
     @PutMapping("/updateProduct/{id}")
-    public void updateProduct(@RequestBody Product product ,@PathVariable Long id) {
+    public String updateProduct(@RequestBody Product product ,@PathVariable Long id, Model model) {
         productService.updateProduct(product, id);
+        return "";
     }
 
     @DeleteMapping("/deleteProduct/{id}")
-    public void deleteProduct(@PathVariable Long id) {
+    public String deleteProduct(@PathVariable Long id , Model model) {
         productService.deleteProduct(id);
+        return "";
     }
 
 }
